aef4d0f9fc81b080a86b2ee62a3c6931eea780bf
Hyun Young Lee <hylee.v1@gmail.com>
2021-05-21 17:29:51 +0100

    Add environment variables file for Contentful access tokens

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,18 @@
+require("dotenv").config({
+  path: `.env.${process.env.NODE_ENV}`,
+})
+
 module.exports = {
   siteMetadata: {
     title: "Portfolio",
   },
-  plugins: [],
+  plugins: [
+    {
+      resolve: "gatsby-source-contentful",
+      options: {
+        accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
+        spaceId: process.env.CONTENTFUL_SPACE_ID,
+      },
+    }
+  ],
 };
