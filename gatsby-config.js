b6b1a09325ca293cf8472b565d7477e578384761
Hyun Young Lee <hylee.v1@gmail.com>
2021-06-12 14:59:17 +0100

    Attempt to fix deployment to netlify/gatsby cloud error

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,12 +7,15 @@ module.exports = {
     title: "Hyun Young Lee",
   },
   plugins: [
+    `gatsby-plugin-image`,
+    `gatsby-plugin-sharp`,
+    `gatsby-transformer-sharp`,
     {
       resolve: "gatsby-source-contentful",
       options: {
         accessToken: process.env.REACT_APP_CONTENTFUL_ACCESS_TOKEN,
         spaceId: process.env.REACT_APP_CONTENTFUL_SPACE_ID,
       },
-    }
+    },
   ],
 };
--- src/components/organisms/abouthyun/index.js
+++ src/components/organisms/abouthyun/index.js
@@ -6,13 +6,6 @@ const AboutHyun = () => {
     const { mainImage, heading, bio } = useAuthorData();
     return (
         <div>
-            {/* <div className="hyun-container">
-                <img src={mainImage.fluid.src} alt="Hyun standing on beach" className="hyun-photo" />
-                <div className="hyun-bio">
-                    <h1 className="hyun-heading">{heading}</h1>
-                    <p>{bio.bio}</p>
-                </div>
-            </div> */}
             <h1 className="hyun-heading">{heading}</h1>
             <img src={mainImage.fluid.src} alt="Hyun standing on beach" className="hyun-photo" />
             <p>{bio.bio}</p>
