9b2c7dcebecfbf669e7c676eb65833fae1351e6c
Hyun Young Lee <hylee.v1@gmail.com>
2021-06-29 23:07:38 +0100

    Refactor typewriter component, partially fix footer placement - change typed text from string to array and refactor code to make it easier to understand - fix issue with footer always falling off the bottom of the screen - padding-bottom for page-container (below footer) not working for scrolling pages

--- src/components/typewriter.js
+++ src/components/typewriter.js
@@ -1,17 +1,39 @@
 import React, { useState, useEffect } from 'react';
 
 const Typewriter = () => {
-    const srcText = `web developer   project manager   writer `;
-    const [{ content, index }, setContent] = useState({ content: '', index: 0 });
+    const initialList = ['web developer', ' project manager', ' writer '];
+    const [roleList, setRoleList] = useState(initialList);
+    const [content, setContent] = useState('');
+    const [index, setIndex] = useState(0);
     const [blink, setBlink] = useState(true);
+    const [currentRole, setCurrentRole] = useState('');
+
+    // sets next item in array as currentRole 
+    const getNewWord = () => {
+        if (!roleList.length) { return; }
+        setTimeout(() => {
+            // deletes item in array after it has been typed out
+            // sets next item as currentRole to be typed out
+            const role = roleList.splice(0, 1)[0];
+            setRoleList([...roleList]);
+            setIndex(0);
+            setCurrentRole(role);
+        }, (Math.random() * 750 + 50))
+    };
 
     useEffect(() => {
-        if (index === srcText.length) return;
+        // fetches next item in array if currentRole typed out completely
+        if (index === currentRole.length) {
+            getNewWord();
+            return;
+        }
 
-        const delay = setTimeout(() => {
-            setContent({ content: content + srcText[index], index: index + 1 });
-        }, 0 | (Math.random() * 150 + 50));
-    }, [content]);
+        // types out each letter/space in currentRole
+        setTimeout(() => {
+            setIndex(index + 1);
+            setContent(content + currentRole[index]);
+        }, (Math.random() * 150 + 50));
+    }, [content, currentRole]);
 
     // blinker
     useEffect(() => {
